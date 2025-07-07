045c8cf788110d33d2aa6990413302b115463988
Andre Dominic Lacra <andre-d.lacra@GXCHLAP00688.local>
2025-07-07 16:35:44 +0800

    feat: add finished codes for GroceryStore

---
+++ src/main/java/org/example/GroceryOperations.java
@@ -0,0 +1,55 @@
+package org.example;
+
+import java.util.HashMap;
+
+public class GroceryOperations {
+    //  private hashmap 'inventory' to store item and quantity <Item (String), Integer (Quantity)>
+    private HashMap<String, Integer> inventory = new HashMap<>();
+
+    //  add an item in the inventory hashmap with method parameters of item name and its quantity
+    public void addProduct (String item, int quant) {
+        inventory.put(item, quant);
+    }
+
+    //  utilized containsKey() with 'item' as parameters to check if that item (key) is in the HashMap
+    // this time we want to return boolean to validate product in st
+    public boolean validateProduct (String item) {
+        return inventory.containsKey(item);
+    }
+
+    //  utilized containsKey() with 'item' as parameters to check if that item (key) is in the HashMap
+    public void checkProduct (String item) {
+        if (inventory.containsKey(item)) {
+            System.out.printf("\n%s is in stock: %s\n\n", item, (inventory.get(item)));
+        }
+        else {
+            System.out.printf("\nItem [%s] isn't available. Item might not included/wrong spelling.\n\n", item);
+        }
+    }
+
+    //  update inventory using .put() to edit item with new quantity stock
+    public void updateProduct (String item, int quant) {
+        inventory.put(item, quant);
+    }
+
+    //  delete item in inventory using .remove()
+    public void removeProduct (String item) {
+        inventory.remove(item);
+    }
+
+    //  check first if the inventory hashmap is empty, if not, will print out each item using lambda
+    public void viewInventory() {
+        if (inventory.isEmpty()) {
+            System.out.println("\n* empty inventory *\n");
+        }
+        else {
+            System.out.println("\nCurrent Inventory:");
+            inventory.forEach((item, quantity) -> {
+                System.out.println(item + " - " + quantity);
+            });
+            System.out.println();
+        }
+
+    }
+
+}
---
+++ src/main/java/org/example/GroceryStore.java
@@ -0,0 +1,114 @@
+package org.example;
+
+import java.util.Scanner;
+
+
+public class GroceryStore {
+    public static void main(String[] args) {
+        //  define scanner object
+        Scanner scannerObj = new Scanner(System.in);
+
+        GroceryOperations groceryOperations = new GroceryOperations();
+
+        //  start program
+        while (true) {
+            //  print out menu board for users to select operation
+            menuBoard();
+
+            //  user operation selection
+            System.out.print("Choose an option (1-6): ");
+            int optionInput = scannerObj.nextInt();
+
+            switch (optionInput) {
+                case 1:
+                    groceryOperations.viewInventory();
+                    break;
+                case 2:
+                    //  user inputs for item & quantity
+                    System.out.print("\nEnter product name: ");
+                    String itemInput = scannerObj.next();
+
+                    System.out.print("Enter product quantity: ");
+                    int quantityInput = scannerObj.nextInt();
+
+                    //  add item & quantity to hashmap
+                    groceryOperations.addProduct(itemInput, quantityInput);
+
+                    System.out.printf("** Item [%s] has been added to inventory. **\n\n", itemInput);
+                    break;
+                case 3:
+                    System.out.print("Enter product name to check: ");
+                    String checkProductInput = scannerObj.next();
+
+                    groceryOperations.checkProduct(checkProductInput);
+                    break;
+                case 4:
+                    //  print out current inventory for users to select an item to update
+                    groceryOperations.viewInventory();
+
+                    while (true) {
+                        //  product name input to update
+                        System.out.print("Enter product name to check: ");
+                        String productNameInput = scannerObj.next();
+
+                        boolean isValidProduct = groceryOperations.validateProduct(productNameInput);
+
+                        if (isValidProduct) {
+                            //  product stock input to update
+                            System.out.print("Enter new stock quantity: ");
+                            int productQuantityInput = scannerObj.nextInt();
+
+                            groceryOperations.updateProduct(productNameInput, productQuantityInput);
+                            System.out.printf("** Item [%s] stock has been updated to -> [%s]. **\n\n", productNameInput, productQuantityInput);
+                            //  break out while loop when product has been updated
+                            break;
+                        }
+                        else {
+                            System.out.printf("\nItem [%s] isn't available. Item might not included/wrong spelling.\n\n", productNameInput);
+                        }
+                    }
+                    break;
+                case 5:
+                    //  print out current inventory for users to select an item to update
+                    groceryOperations.viewInventory();
+
+                    //  product name input to update
+                    System.out.print("Enter product name to remove: ");
+                    String productNameInput = scannerObj.next();
+
+                    boolean isValidProduct = groceryOperations.validateProduct(productNameInput);
+
+                    while (true) {
+                        if (isValidProduct) {
+                            groceryOperations.removeProduct(productNameInput);
+                            System.out.printf("** Item [%s] has been removed from inventory. **\n\n", productNameInput);
+                            //  break out while loop when product has been updated
+                            break;
+                        }
+                        else {
+                            System.out.printf("\nItem [%s] isn't available. Item might not included/wrong spelling.\n\n", productNameInput);
+                        }
+                    }
+                    break;
+                case 6:
+                    System.exit(0);
+                    break;
+            }
+        }
+
+    }
+    //  method to store the grocery user guide UI menu board
+    public static void menuBoard() {
+        System.out.println("--- Grocery Inventory Menu ---");
+        System.out.println(
+                """
+                1. View Inventory
+                2. Add Product
+                3. Check Product
+                4. Update Stock
+                5. Remove Product
+                6. Exit
+                """
+        );
+    }
+}
